96540b43fb130a0c12fe5b02c8aa406cc78b8ede
Michael VanSleen <mike.vansleen@gmail.com>
2020-01-17 11:29:37 -0700

    PlayerInfo styled

--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "./SideBar.scss";
+
 import PlayerInfo from "./PlayerInfo";
 import Wearables from "./Wearables";
 import RoomInfo from "./RoomInfo";
 
 const SideBar = () => {
   return (
-    <div>
+    <div className="sidebar">
       <PlayerInfo />
       <Wearables />
       <RoomInfo />
