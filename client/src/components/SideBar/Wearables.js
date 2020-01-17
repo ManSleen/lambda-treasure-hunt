4003fb2df69bb6ce07a626f09a3555455b04e55a
Michael VanSleen <mike.vansleen@gmail.com>
2020-01-17 11:56:25 -0700

    Wearables initial styling done

--- client/src/components/SideBar/Wearables.js
+++ client/src/components/SideBar/Wearables.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import upperBody from "../../images/upper-body.svg";
+import lowerBody from "../../images/lower-body.svg";
+
 const Wearables = () => {
-  return <div>Wearables</div>;
+  return (
+    <div className="wearables-container">
+      <div className="upper-body child">
+        <img src={upperBody} alt-="" />
+        <p>terrible jacket</p>
+      </div>
+      <div className="lower-body child">
+        <img src={lowerBody} alt-="" />
+        <p>nice boots</p>
+      </div>
+    </div>
+  );
 };
 
 export default Wearables;
