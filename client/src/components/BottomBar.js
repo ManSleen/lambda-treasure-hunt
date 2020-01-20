3d4b1f6f48537ea0d633022f36dea547ec3e63b0
Michael VanSleen <mike.vansleen@gmail.com>
2020-01-20 09:34:45 -0700

    BottomBar looking good

--- client/src/components/BottomBar.js
+++ client/src/components/BottomBar.js
@@ -1,23 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./BottomBar.scss";
+
 import backpack from "../images/backpack.svg";
 import pickaxe from "../images/pickaxe.svg";
 import warp from "../images/warp.svg";
+import dna from "../images/dna.svg";
 
 const BottomBar = () => {
   return (
     <div className="bottom-bar-container">
       <div className="action-button">
-        <img src={backpack} alt="" />
-        Inventory
+        <Link>
+          <img src={backpack} alt="" />
+          Inventory
+        </Link>
+      </div>
+
+      <div className="action-button">
+        <Link>
+          <img src={pickaxe} alt="" />
+          Mine
+        </Link>
       </div>
       <div className="action-button">
-        <img src={pickaxe} alt="" />
-        Mine
+        <Link>
+          <img src={warp} alt="" />
+          Warp
+        </Link>
       </div>
       <div className="action-button">
-        <img src={warp} alt="" />
-        Warp
+        <Link>
+          <img src={dna} alt="" />
+          Abilities
+        </Link>
       </div>
     </div>
   );
