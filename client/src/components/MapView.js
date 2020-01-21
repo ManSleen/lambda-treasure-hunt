9d0936902cc6210c45e491524be8c7130bb36b18
Michael VanSleen <mike.vansleen@gmail.com>
2020-01-21 08:37:02 -0700

    Map is rendering but needs a lot of work

--- client/src/components/MapView.js
+++ client/src/components/MapView.js
@@ -1,8 +1,42 @@
 import React from "react";
+import map from "../map/map.json";
 import "./MapView.scss";
 
+const roomStyle = {
+  backgroundColor: "coral",
+  width: "50px",
+  height: "50px"
+};
+
+const rooms = [];
+
+const generateMap = graph => {
+  for (const room in graph) {
+    const str = graph[room].coordinates;
+    const x = str.split(",")[0].slice(1);
+    const y = str.split(",")[1].slice(0, -1);
+
+    rooms.push(
+      <div
+        style={{
+          backgroundColor: "coral",
+          width: "40px",
+          height: "40px",
+          top: `${y * 60}px`,
+          left: `${x * 60}px`
+        }}
+        className="map-child"
+      >
+        {map[room].room_id}
+      </div>
+    );
+  }
+};
+generateMap(map);
+console.log(rooms);
 const MapView = () => {
-  return <div className="map-view-container">Map View</div>;
+  // console.log("map: ", map);
+  return <div className="map-view-container">{rooms}</div>;
 };
 
 export default MapView;
